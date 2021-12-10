42f4df4cb170b6b92c87ac0a0dc92c4509bbd578
Arden Johannes Sasan <ajsasan@dnamicro.com>
2021-12-10 13:29:15 +0800

    delete default reducer

--- src/common/reducer.ts
+++ src/common/reducer.ts
@@ -2,10 +2,6 @@ import { combineReducers } from 'redux'
 import user from "../components/User/reducer"
 //REDUCER IMPORTS
 
-const defaultReducer = () =>{
-    
-}
-
 const allReducer = combineReducers({
 user : user, 
 //REDUCERS
