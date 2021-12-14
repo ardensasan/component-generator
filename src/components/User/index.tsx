9ba9dfed159a89abe459d0ee49e034f12fca3038
Arden Johannes Sasan <ajsasan@dnamicro.com>
2021-12-14 08:35:21 +0800

    add dialog handlebars

--- src/components/User/index.tsx
+++ src/components/User/index.tsx
@@ -1,17 +1,10 @@
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button} from '@mui/material'
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, Fragment } from "react";
 import { toTitleCase } from "../../utils/string";
 import { deleteUser, getUserDetails, getUserList, openDialog } from "./action";
-import fields from "./tableFields";
 import DialogForm from "../../common/components/DialogForm";
-import { Button } from "@mui/material";
+import fields from "./tableFields";
 //TODO ADD EDIT
 const User = () => {
   const dispatch = useDispatch();
